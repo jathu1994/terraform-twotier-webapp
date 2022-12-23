6c0f1792f066aa6a63c9a56bad73861ec5da4eb1
Jatharthan Tharmanandasivam <jathu10061994@gmail.com>
2022-12-23 23:43:22 +0000

    adding dev environment attributes

--- environments/dev/Webserver/variables.tf
+++ environments/dev/Webserver/variables.tf
@@ -1,17 +1,41 @@
+# Instance type
 variable "instance_type" {
   default     = "t3.micro"
-  description = "Instance Type for dev deployment"
+  description = "Type of the instance"
   type        = string
 }
 
+
+# Variable to signal the current environment 
 variable "env" {
-  default     = "dev"
+  default     = "staging"
   type        = string
-  description = "Environment"
+  description = "Deployment Environment"
 }
 
-variable "linux_key" {
+#Desired Capacity 
+variable "desired_size" {
+  default     = 2
+  type        = number
+  description = "Optimal Capacity"
+}
+
+#Minimum group size for auto scaling
+variable "minimum_size" {
+  default     = 2
+  type        = number
+  description = "Minimum group size for auto scaling"
+}
+#Maximum group size for auto scaling
+variable "maximum_size" {
+  default     = 3
+  type        = number
+  description = "Maximum group size for auto scaling"
+}
+
+
+variable "path_to_ssh_key" {
   default     = "/home/ec2-user/.ssh/dev.pub"
-  description = "Dev Key"
+  description = "Path to the public key to use in Linux VMs provisioning"
   type        = string
 }

045aea5197ea792657b7068474e590eee2a7974a
Chathumi <chathumirambukwella@gmail.com>
2022-12-23 13:12:45 +0000

    adding load balancer

---
+++ global_modules/webserver/load_balancer.tf
@@ -0,0 +1,37 @@
+# Application Load Balancer
+resource "aws_lb" "application_load_balancer" {
+  name               = "ALB-${var.env}"
+  internal           = false //internert facing
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_webserver.id]
+  subnets            = data.terraform_remote_state.network.outputs.public_subnet_ids[*]
+  enable_deletion_protection = false
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.prefix_all}-ALB-${var.env}"
+    }
+  )
+}
+
+# Target Group
+resource "aws_lb_target_group" "targetgroup" {
+  name     = "targetgroup-${var.env}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.network.outputs.vpc_id
+  
+  lifecycle { create_before_destroy = true }
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 25
+    interval            = 30
+  }
+  
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.prefix_all}-targetgroup-${var.env}"
+    }
+  )
+}

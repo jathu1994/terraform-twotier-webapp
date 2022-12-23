e5a1a6bae5f74ea512280cce15433a8fba2a29b4
Jatharthan Tharmanandasivam <jathu10061994@gmail.com>
2022-12-23 23:17:04 +0000

    correction to s3 naming which is referenced by webserver

--- environments/prod/Networking/config.tf
+++ environments/prod/Networking/config.tf
@@ -1,7 +1,7 @@
 # Referencing the s3 bucket for the remote state storage 
 terraform {
   backend "s3" {
-    bucket = "group10-acs730-prod-bucket"
+    bucket = "prod-finalproject-acs730-group10"
     key    = "prod-Network/terraform.tfstate"
     region = "us-east-1"
   }
--- environments/prod/Networking/main.tf
+++ environments/prod/Networking/main.tf
@@ -5,7 +5,6 @@ module "network-prod" {
   vpc_cidr      = var.vpc_cidr
   public_cidrs  = var.public_cidrs
   private_cidrs = var.private_cidrs
-  # prefix       = module.global_variables.prefix
   default_tags = module.global_variables.default_tags
 }
 

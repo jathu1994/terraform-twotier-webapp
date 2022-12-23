3f9e6e548feb6f7d34ae52703b86df79298eb066
Jatharthan Tharmanandasivam <jathu10061994@gmail.com>
2022-12-23 23:46:25 +0000

    updating s3 name

--- environments/staging/Networking/config.tf
+++ environments/staging/Networking/config.tf
@@ -1,7 +1,7 @@
 # Referencing the s3 bucket for the remote state storage 
 terraform {
   backend "s3" {
-    bucket = "group10-acs730-staging-bucket"
+    bucket = "staging-finalproject-acs730-group10"
     key    = "staging-Network/terraform.tfstate"
     region = "us-east-1"
   }

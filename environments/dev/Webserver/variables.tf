be8e626666cee363d4a4fc288bd86cecbf82cce5
Jatharthan Tharmanandasivam <jathu10061994@gmail.com>
2022-12-24 01:37:06 +0000

    cidr correction and troubleshooting

--- environments/dev/Webserver/variables.tf
+++ environments/dev/Webserver/variables.tf
@@ -8,7 +8,7 @@ variable "instance_type" {
 
 # Variable to signal the current environment 
 variable "env" {
-  default     = "staging"
+  default     = "dev"
   type        = string
   description = "Deployment Environment"
 }
--- environments/staging/Networking/variables.tf
+++ environments/staging/Networking/variables.tf
@@ -7,21 +7,21 @@ variable "env" {
 
 # variable for defining the public subnet range for public subnets
 variable "public_cidrs" {
-  default     = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
+  default     = ["10.200.1.0/24", "10.200.2.0/24", "10.200.3.0/24"]
   type        = list(string)
   description = "Public Subnet range"
 }
 
 # variable for defining the private subnet range for public subnets
 variable "private_cidrs" {
-  default     = ["10.100.4.0/24", "10.100.8.0/24", "10.100.9.0/24"]
+  default     = ["10.200.4.0/24", "10.200.5.0/24", "10.200.6.0/24"]
   type        = list(string)
   description = "Private Subnet range"
 }
 
 # variable for defining the vpc cidr network address 
 variable "vpc_cidr" {
-  default     = "10.100.0.0/16"
+  default     = "10.200.0.0/16"
   type        = string
   description = "VPC CIDR range"
 }
